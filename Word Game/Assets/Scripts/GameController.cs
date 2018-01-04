8f0b0d9e232c4749e4b726922bcb5f7d8a2e5a8f
Jacker71300 <hoffmanjack713@gmail.com>
2018-01-04 12:44:46 -0500

    tons of changes made, ui now works
    sort of

--- Word Game/Assets/Scripts/GameController.cs
+++ Word Game/Assets/Scripts/GameController.cs
@@ -8,10 +8,13 @@ public class GameController : MonoBehaviour {
 	// Instance Variables
 	public ArrayList listWords = new ArrayList();
 	public ArrayList letterPics = new ArrayList();
-	public Transform Letter = null;
+	public Transform Letter;
+	public Transform canvas;
 
-	private int index;
-	private string currentWord;
+	private int currentIndex = 0;
+	private string guess = "";
+	private int index = 0;
+	private string currentWord = "";
 	// Use this for initialization
 	void Start () {
 		string fileDirectory = System.IO.Directory.GetCurrentDirectory ();
@@ -35,24 +38,63 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(guess.Equals(currentWord)){
+			WordSuccess();
+		}
 	}
 
 	void WordSuccess(){
+		currentIndex++;
+		try{
+			CreateWord ();
+		}
+		catch{
+			Win ();
+		}
+	}
+
+	void Win(){
 
 	}
 
 	void CreateWord(){
-		currentWord = (string) listWords [0];
-		listWords.RemoveAt (0);
+		currentWord = (string) listWords [currentIndex];
+		listWords.RemoveAt (currentIndex);
 		currentWord = currentWord.ToLower ();
 
 		char[] letters = currentWord.ToCharArray ();
 
 
 		for(int i = 0; i < letters.Length; i++){
-			Instantiate (Letter, new Vector3 (100f * i, 200f), Quaternion.identity);
+			Transform holder = Instantiate (Letter, new Vector3 (100f * i + 100f, 75f), Quaternion.identity) as RectTransform;
+			holder.SetParent (canvas);
+			string filename = letters [i].ToString ().ToUpper ();
+			holder.GetComponent<Image> ().sprite = Resources.Load<Sprite>(filename);
+			print (filename);
+			letterPics.Add (holder);
 			print (letters [i]);
+		}
+	}
+
+	public void addChar(GameObject letterbox){
+		string name = letterbox.GetComponent<Image> ().sprite.name;
+		guess += name;
+	}
+
+	public void removeChar(GameObject letterbox){
+		string name = letterbox.GetComponent<Image> ().sprite.name;
+		char[] holder = guess.ToCharArray ();
+
+		for(int i = holder.Length; i >= 0; i--){
+			if (holder [i] == name.ToCharArray () [0])
+				holder [i] = '|';
+		}
+
+		guess = "";
+
+		for (int i = 0; i < holder.Length; i++) {
+			if (holder [i] != '|')
+				guess += holder [i];
 		}
 	}
 }
